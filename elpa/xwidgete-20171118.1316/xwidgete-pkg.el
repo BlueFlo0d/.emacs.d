554fd292e2adb22c41b74a18ee30f8036aa16fe0
less <hongqiantan@pku.edu.cn>
2018-01-30 17:04:49 +0800

    initial

---
+++ elpa/xwidgete-20171118.1316/xwidgete-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "xwidgete" "20171118.1316" "enhances usability of current xwidget browser" '((emacs "25")) :commit "e4e8410fe32176df85b46234717824519443fb04" :url "https://github.com/tuhdo/xwidgete" :keywords '("xwidgete" "tools"))
